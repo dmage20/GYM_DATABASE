7251be557fdfcf5ee946d3f29c74e6d4807f4175
Daniel Mage <dmage20@gmail.com>
2018-06-09 15:08:27 +0800

    Gym seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+filepath = 'crossfit_locations.json'
+
+serialized_gyms = File.read(filepath)
+gyms = JSON.parse(serialized_gyms)
+gyms.each do |gym|
+  name = gym["name"]
+  address = gym["address"]
+  country = gym["country"]
+  city = gym["city"]
+  box = Gym.new(name: name, address: address, country: country, city: city)
+  box.save!
+end
+
