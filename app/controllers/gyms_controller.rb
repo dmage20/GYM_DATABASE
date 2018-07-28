6fca04edfe70d35d79d33010a7556e0c467d9692
Daniel Mage <dmage20@gmail.com>
2018-07-28 20:45:11 +0800

    there is a link for instagram on the gym showpage

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -5,68 +5,6 @@ class GymsController < ApplicationController
   require 'savon'
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-  # def class_schedule
-  #   # Setup credentials
-  #   site_ids = { 'int' => -99 } # Use your site ID here
-  #   source_credentials = {
-  #     'SourceName' => 'gngbase',
-  #     'Password' => 'VtA5jsaF3ShKtoaw2e2/tBepZLU=',
-  #     'SiteIDs' => site_ids
-  #   }
-  #   user_credentials = {
-  #     'Username' => 'Siteowner',
-  #     'Password' => 'apitest1234',
-  #     'SiteIDs' => site_ids
-  #   }
-  #     #######################
-  #     ## Standard API call ##
-  #     #######################
-
-  #     # Create Savon client using default settings
-  #     http_client = Savon.client(wsdl: "https://api.mindbodyonline.com/0_5/ClassService.asmx?WSDL")
-
-  #     # Create request and package it for the call
-  #     http_request = {
-  #       'SourceCredentials' => source_credentials,
-  #       'UserCredentials' => user_credentials
-  #     }
-  #     params = { 'Request' => http_request }
-
-  #     # Fetch current day's classes via GetClasses
-  #     # https://developers.mindbodyonline.com/Documentation/ClassService?version=v5.0
-  #     result = http_client.call(:get_classes, message: params)
-
-  #     # Parse class data
-  #     class_data = result.body.dig(:get_classes_response, :get_classes_result, :classes, :class)
-
-  #     formatted_classes = class_data.map do |scheduled_class|
-  #       {
-  #         name: scheduled_class.dig(:class_description, :name),
-  #         description: scheduled_class.dig(:class_description, :description),
-  #         start_date_time: scheduled_class[:start_date_time].strftime("%B %d, %Y %l:%M%P"),
-  #         end_date_time: scheduled_class[:end_date_time].strftime("%B %d, %Y %l:%M%P")
-  #       }
-  #     end
-
-  #     # Display class info
-  #     puts "Class schedule:"
-  #     @location_schedule = []
-  #     formatted_classes.each do |formatted_class|
-  #       # puts "Name: #{formatted_class[:name]} (#{formatted_class[:start_date_time]} - #{formatted_class[:end_date_time]})"
-  #       # puts "Description: #{formatted_class[:description]}"
-  #       # puts "-----------------------------------------------------------"
-  #       each_class = {
-  #       "class_name" => "#{formatted_class[:name]}",
-  #       "start_date_time" => "#{formatted_class[:start_date_time]}",
-  #       "end_date_time" => "#{formatted_class[:end_date_time]}",
-  #       "description" => "#{formatted_class[:description]}"
-  #     }
-  #     @location_schedule << each_class
-
-  #     end
-
-  # end
-
   def show
     @gyms = []
     @gym = Gym.find(params["id"])
@@ -244,3 +182,65 @@ end
 
 # sql_query = Country.where(name: "Colombia")
 # Gym.joins(:country).where('countries.name' => "Colombia")
+
+  # def class_schedule
+  #   # Setup credentials
+  #   site_ids = { 'int' => -99 } # Use your site ID here
+  #   source_credentials = {
+  #     'SourceName' => 'gngbase',
+  #     'Password' => 'VtA5jsaF3ShKtoaw2e2/tBepZLU=',
+  #     'SiteIDs' => site_ids
+  #   }
+  #   user_credentials = {
+  #     'Username' => 'Siteowner',
+  #     'Password' => 'apitest1234',
+  #     'SiteIDs' => site_ids
+  #   }
+  #     #######################
+  #     ## Standard API call ##
+  #     #######################
+
+  #     # Create Savon client using default settings
+  #     http_client = Savon.client(wsdl: "https://api.mindbodyonline.com/0_5/ClassService.asmx?WSDL")
+
+  #     # Create request and package it for the call
+  #     http_request = {
+  #       'SourceCredentials' => source_credentials,
+  #       'UserCredentials' => user_credentials
+  #     }
+  #     params = { 'Request' => http_request }
+
+  #     # Fetch current day's classes via GetClasses
+  #     # https://developers.mindbodyonline.com/Documentation/ClassService?version=v5.0
+  #     result = http_client.call(:get_classes, message: params)
+
+  #     # Parse class data
+  #     class_data = result.body.dig(:get_classes_response, :get_classes_result, :classes, :class)
+
+  #     formatted_classes = class_data.map do |scheduled_class|
+  #       {
+  #         name: scheduled_class.dig(:class_description, :name),
+  #         description: scheduled_class.dig(:class_description, :description),
+  #         start_date_time: scheduled_class[:start_date_time].strftime("%B %d, %Y %l:%M%P"),
+  #         end_date_time: scheduled_class[:end_date_time].strftime("%B %d, %Y %l:%M%P")
+  #       }
+  #     end
+
+  #     # Display class info
+  #     puts "Class schedule:"
+  #     @location_schedule = []
+  #     formatted_classes.each do |formatted_class|
+  #       # puts "Name: #{formatted_class[:name]} (#{formatted_class[:start_date_time]} - #{formatted_class[:end_date_time]})"
+  #       # puts "Description: #{formatted_class[:description]}"
+  #       # puts "-----------------------------------------------------------"
+  #       each_class = {
+  #       "class_name" => "#{formatted_class[:name]}",
+  #       "start_date_time" => "#{formatted_class[:start_date_time]}",
+  #       "end_date_time" => "#{formatted_class[:end_date_time]}",
+  #       "description" => "#{formatted_class[:description]}"
+  #     }
+  #     @location_schedule << each_class
+
+  #     end
+
+  # end
