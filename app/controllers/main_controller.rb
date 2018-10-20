e71a5b36509dfd117bad4948c4959db0f159dc69
Daniel Mage <dmage20@gmail.com>
2018-10-20 13:16:36 -0500

    inital change

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,11 @@
+class MainController < ApplicationController
+      @users = User.where("name ILIKE ?", "%#{params[:query]}%")
+    # render json: {users:[]}
+    respond_to do |format|
+      format.html {}
+      format.json {
+        @users = @users.limit(5)
+      }
+    end
+  end
+end
