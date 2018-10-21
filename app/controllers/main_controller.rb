c46a870f43eb1a28fa84b6e36ecfd28e6594240e
Daniel Mage <dmage20@gmail.com>
2018-10-21 08:04:24 -0500

    return json

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,13 @@
 class MainController < ApplicationController
+  def search
       @users = User.where("name ILIKE ?", "%#{params[:query]}%")
     # render json: {users:[]}
-    respond_to do |format|
+      respond_to do |format|
       format.html {}
       format.json {
         @users = @users.limit(5)
       }
     end
   end
+
 end
