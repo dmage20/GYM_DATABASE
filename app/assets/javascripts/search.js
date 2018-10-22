f054448a3255416f44cb412317bffb0346837f21
Daniel Mage <dmage20@gmail.com>
2018-10-22 02:25:54 -0500

    messages work

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,38 @@
+// document.addEventListener("turbolinks:load", function() {
+document.addEventListener("DOMContentLoaded", function() {
+  $input = $("#basics")
+
+  var options = {
+    getValue: "name",
+    url: function(phrase){
+      return "/search.json?query=" + phrase;
+    },
+    categories: [
+    {
+      listLocation: "users",
+      header: "<strong>USERS</strong>",
+    }
+    ],
+    list: {
+
+      onChooseEvent: function() {
+        var url = $input.getSelectedItemData().url
+        // Turbolinks.visit(url)
+        // $input.val("")
+        // console.log(url)
+        $.post(url)
+
+        // $.ajax({
+        //   type: "POST",
+        //   url: url,
+        //   dataType: "html"
+        //   });
+
+        // window.location.reload(true);
+        // window.location.href = `${url}`;
+
+      }
+    }
+  }
+  $input.easyAutocomplete(options)
+});
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,19 +2,30 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+    # Conversation.where("(conversations.sender_id = ? OR conversations.recipient_id =?)", current_user.id, current_user.id)
   end
-def create
- if Conversation.between(params[:sender_id],params[:recipient_id]).present?
-  @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
- else
-  @conversation = Conversation.create!(conversation_params)
+  def create
+    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    redirect_to conversation_messages_path(@conversation)
+    end
 
- end
- redirect_to conversation_messages_path(@conversation)
-end
 
-private
- def conversation_params
-  params.permit(:sender_id, :recipient_id)
- end
+  end
+  def destroy
+
+      @conversation = Conversation.where(conversation_params)
+      @conversation.destroy_all
+
+      redirect_to conversations_path
+  end
+
+  private
+
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
+
 end
